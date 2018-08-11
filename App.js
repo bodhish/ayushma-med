bd6f809b15faeafbc1a64e1bf5d479607cfe1a79
Bodhish Thomas <bodhish@gmail.com>
2018-08-11 22:55:35 +0530

    Add ScrollView

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
 import { green } from "./node_modules/ansi-colors";
 
 export default class App extends React.Component {
@@ -7,7 +7,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.app__root}>
         <View style={styles.app__headder} />
-        <View style={styles.app__body}>
+        <ScrollView style={styles.app__body}>
           {/* {<Image source={require("./res/logo.png")} style={styles.app__headerLogo} />} */}
           <Text
             style={{
@@ -16,15 +16,11 @@ export default class App extends React.Component {
           >
             Medication Reminder
           </Text>
-          <View style={styles.app__cards}>  
-          </View>
-          <View style={styles.app__cards}>  
-          </View>
-          <View style={styles.app__cards}>  
-          </View>
-          <View style={styles.app__cards}>  
-          </View>
-        </View>
+          <View style={styles.app__cards} />
+          <View style={styles.app__cards} />
+          <View style={styles.app__cards} />
+          <View style={styles.app__cards} />
+        </ScrollView>
         <View style={styles.app__footer} />
       </View>
     );
@@ -37,8 +33,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff"
   },
   app__headder: { flex: 1, backgroundColor: "#39be88" },
-  app__body: { flex: 8, backgroundColor: "#fff" },
+  app__body: { flex: 8, flexDirection: 'column', backgroundColor: "#fff" },
   app__footer: { flex: 1, marginTop: 20, backgroundColor: "#27885f" },
   app__headderLogo: { height: 1, width: 1 },
-  app__cards: { flex:1, backgroundColor: "#000", maxHeight: 150}
+  app__cards: { flex: 1, marginTop:2, marginBottom: 2, backgroundColor: "#000", maxHeight: 200 }
 });
