f5a0d1b906684536379c53b4f02c5c5defc9d34e
Bodhish Thomas <bodhish@gmail.com>
2018-08-13 02:07:35 +0530

    WIP add text input & buttons

--- App.js
+++ App.js
@@ -1,8 +1,26 @@
 import React from "react";
-import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Image,
+  Button,
+  ScrollView
+} from "react-native";
 import { green } from "./node_modules/ansi-colors";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "Add your med name", time: 1 };
+    this.saveInput = this.saveInput.bind(this);
+  }
+
+  saveInput(event) {
+    perviousValue = this.state.time
+    this.setState({ time: perviousValue + 1 });
+  }
   render() {
     return (
       <View style={styles.app__root}>
@@ -22,16 +40,39 @@ export default class App extends React.Component {
               style={styles.app__headerLogo}
             />
           } */}
-
+          <Text
+            style={{
+              fontSize: 20
+            }}
+          >
+            {this.state.text},{this.state.time}
+          </Text>
           <View style={styles.app__cards}>
-            <Text
-              style={{
-                fontSize: 20
-              }}
-            >
-              Medication Reminde new
-            </Text>
+            <TextInput
+              style={{ height: 40, margin: 5 }}
+              onChangeText={text => this.setState({ text })}
+              value={this.state.text}
+            />
+            <Button
+              onPress={this.saveInput}
+              title="Morning"
+              color="#27885f"
+              accessibilityLabel="Learn more about this purple button"
+            />
+             <Button
+              onPress={this.saveInput}
+              title="Afternoon"
+              color="#27885f"
+              accessibilityLabel="Learn more about this purple button"
+            />
+             <Button
+              onPress={this.saveInput}
+              title="Evening"
+              color="#27885f"
+              accessibilityLabel="Learn more about this purple button"
+            />
           </View>
+          <View style={styles.app__cards} />
           <View style={styles.app__cards} />
           <View style={styles.app__cards} />
           <View style={styles.app__cards} />
@@ -60,9 +101,9 @@ const styles = StyleSheet.create({
   },
   app__headderLogo: { height: 1, width: 1 },
   app__cards: {
-    height: 200,
+    height: 150,
     margin: 4,
-    backgroundColor: "#3abf89",
-    maxHeight: 200,
+    backgroundColor: "#ecebeb",
+    maxHeight: 200
   }
 });
