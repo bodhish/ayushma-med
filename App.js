8a268fc1bb42fca575ef52d64e9fd48af406d957
Bodhish Thomas <bodhish@gmail.com>
2018-08-12 21:33:35 +0530

    Add React Navigation

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ export default class App extends React.Component {
               fontSize: 20
             }}
           >
-            Medication Reminder  new
+            Medication Reminder
           </Text>
           <View style={styles.app__cards} >
           <Text
