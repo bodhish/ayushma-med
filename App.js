4885bc788659f41781eb299de5290f50a5134a7a
Bodhish Thomas <bodhish@gmail.com>
2018-08-13 21:08:18 +0530

    Update component naming

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { createStackNavigator } from "react-navigation";
 
-import HomeScreen from "./reminder";
-import DetailsScreen from "./addReminder";
+import Reminder from "./reminder";
+import AddReminder from "./addReminder";
 
 const RootStack = createStackNavigator(
   {
-    Home: HomeScreen,
-    Details: DetailsScreen,
+    Home: Reminder,
+    AddReminder: AddReminder,
   },
   {
     initialRouteName: 'Home',
--- reminder.js
+++ reminder.js
@@ -27,7 +27,7 @@ class HomeScreen extends React.Component {
           title="Go to Details"
           onPress={() => {
             /* 1. Navigate to the Details route with params */
-            this.props.navigation.navigate("Details", {
+            this.props.navigation.navigate("AddReminder", {
               itemId: 86,
               otherParam: "anything you want here"
             });
