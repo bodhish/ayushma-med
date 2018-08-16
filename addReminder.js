b8abc9d396af1db9e9f20846a8ba51f6bb629dc6
Bodhish Thomas <bodhish@gmail.com>
2018-08-16 15:07:25 +0530

    Add picker for date selection

--- addReminder.js
+++ addReminder.js
@@ -7,6 +7,7 @@ import {
   View,
   Text,
   ToastAndroid,
+  Picker,
   TextInput,
   StyleSheet
 } from "react-native";
@@ -25,126 +26,142 @@ class DetailsScreen extends React.Component {
   render() {
     return (
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <View style={styles.addReminder__root}>
-        <View
-          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-        >
-          <View style={styles.addReminder__cards}>
-            <View style={styles.addReminder__cardLeft}>
-              <TextInput
-                underlineColorAndroid="transparent"
-                style={{
-                  color: "#FAFA32",
-                  fontSize: 20,
-                  fontWeight: "bold",
-                  paddingTop: 45,
-                  marginLeft: 10,
-                  borderColor: "#16CB93"
-                }}
-                onChangeText={text => this.setState({ name: text })}
-                placeholder={"✎ Enter your Med name!"}
-              />
-              <View style={styles.addReminder__buttonGroup}>
-                <View style={styles.app__HeaderCircleContainer}>
-                  <TouchableOpacity
-                    style={styles.app__button}
-                    onPress={() => {
-                      this.setState(prevState => ({
-                        morning: !prevState.morning
-                      }));
-                    }}
-                  >
-                    <Text
-                      style={
-                        this.state.morning
-                          ? styles.addReminder__morningTrue
-                          : styles.addReminder__morning
-                      }
+        <View style={styles.addReminder__root}>
+          <View
+            style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+          >
+            <View style={styles.addReminder__cards}>
+              <View style={styles.addReminder__cardLeft}>
+                <TextInput
+                  underlineColorAndroid="transparent"
+                  style={{
+                    color: "#FAFA32",
+                    fontSize: 20,
+                    fontWeight: "bold",
+                    paddingTop: 45,
+                    marginLeft: 10,
+                    borderColor: "#16CB93"
+                  }}
+                  onChangeText={text => this.setState({ name: text })}
+                  placeholder={"✎ Enter your Med name!"}
+                />
+                <View style={styles.addReminder__buttonGroup}>
+                  <View style={styles.app__HeaderCircleContainer}>
+                    <TouchableOpacity
+                      style={styles.app__button}
+                      onPress={() => {
+                        this.setState(prevState => ({
+                          morning: !prevState.morning
+                        }));
+                      }}
                     >
-                      ☼
-                    </Text>
-                  </TouchableOpacity>
-                </View>
-                <View style={styles.app__HeaderCircleContainer}>
-                  <TouchableOpacity
-                    style={styles.app__button}
-                    onPress={() => {
-                      this.setState(prevState => ({
-                        afternoon: !prevState.afternoon
-                      }));
-                    }}
-                  >
-                    <Text
-                      style={
-                        this.state.afternoon
-                          ? styles.addReminder__afternoonTrue
-                          : styles.addReminder__afternoon
-                      }
+                      <Text
+                        style={
+                          this.state.morning
+                            ? styles.addReminder__morningTrue
+                            : styles.addReminder__morning
+                        }
+                      >
+                        ☼
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+                  <View style={styles.app__HeaderCircleContainer}>
+                    <TouchableOpacity
+                      style={styles.app__button}
+                      onPress={() => {
+                        this.setState(prevState => ({
+                          afternoon: !prevState.afternoon
+                        }));
+                      }}
                     >
-                      ◉
-                    </Text>
-                  </TouchableOpacity>
-                </View>
-                <View style={styles.app__HeaderCircleContainer}>
-                  <TouchableOpacity
-                    style={styles.app__button}
-                    onPress={() => {
-                      this.setState(prevState => ({
-                        night: !prevState.night
-                      }));
-                    }}
-                  >
-                    <Text
-                      style={
-                        this.state.night
-                          ? styles.addReminder__nightTrue
-                          : styles.addReminder__night
-                      }
+                      <Text
+                        style={
+                          this.state.afternoon
+                            ? styles.addReminder__afternoonTrue
+                            : styles.addReminder__afternoon
+                        }
+                      >
+                        ◉
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+                  <View style={styles.app__HeaderCircleContainer}>
+                    <TouchableOpacity
+                      style={styles.app__button}
+                      onPress={() => {
+                        this.setState(prevState => ({
+                          night: !prevState.night
+                        }));
+                      }}
                     >
-                      ☾
-                    </Text>
-                  </TouchableOpacity>
+                      <Text
+                        style={
+                          this.state.night
+                            ? styles.addReminder__nightTrue
+                            : styles.addReminder__night
+                        }
+                      >
+                        ☾
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
                 </View>
               </View>
-            </View>
 
-            <View style={styles.addReminder__cardRight}>
-              <View style={styles.addReminder__cardRightCircle}>
-                <View style={styles.circleContainer}>
+              <View style={styles.addReminder__cardRight}>
+                <View style={styles.addReminder__cardRightCircle}>
+                  <View style={styles.circleContainer}>
+                    <Picker
+                      // selectedValue={this.state.numberOfDays}
+                      style={{ height: 50, width: 100 }}
+                      onValueChange={(itemValue, itemIndex) =>
+                        this.setState({ numberOfDays: itemValue })
+                      }
+                    >
+                      <Picker.Item label="1 day" value="1" />
+                      <Picker.Item label="2 days" value="2" />
+                      <Picker.Item label="3 days" value="3" />
+                      <Picker.Item label="4 days" value="4" />
+                      <Picker.Item label="5 days" value="5" />
+                      <Picker.Item label="6 days" value="6" />
+                      <Picker.Item label="1 week" value="7" />
+                    </Picker>
+                    <Text
+                      style={{
+                        color: "#6C6C6C",
+                        fontWeight: "bold",
+                        fontSize: 60
+                      }}
+                    >
+                      {this.state.numberOfDays}
+                    </Text>
+                  </View>
+                </View>
+                <View style={styles.addReminder__cardRightText}>
                   <Text
                     style={{
-                      color: "#6C6C6C",
-                      fontWeight: "bold",
-                      fontSize: 60
+                      color: "#fff",
+                      paddingLeft: 10,
+                      fontSize: 15,
+                      paddingTop: 45
                     }}
                   >
-                    {this.state.numberOfDays}
+                    days
                   </Text>
                 </View>
               </View>
-              <View style={styles.addReminder__cardRightText}>
-                <Text
-                  style={{
-                    color: "#fff",
-                    paddingLeft: 10,
-                    fontSize: 15,
-                    paddingTop: 45
-                  }}
-                >
-                  days
-                </Text>
-              </View>
             </View>
           </View>
+          <Button
+            title="Add Reminder"
+            color="#16CB93"
+            onPress={() => {
+              ToastAndroid.show("We will remind you :)", ToastAndroid.SHORT);
+              this.props.navigation.navigate("Home");
+            }}
+          />
         </View>
-        <Button
-          title="Add Reminder"
-          color="#16CB93"
-          onPress={() => {
-            ToastAndroid.show('We will remind you :)', ToastAndroid.SHORT);
-            this.props.navigation.navigate("Home")}}
-        />
-      </View>
       </TouchableWithoutFeedback>
     );
   }
