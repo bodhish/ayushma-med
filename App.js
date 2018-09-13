886b2730b256c8de6528d8f372e5e67fd9e0c24f
Bodhish Thomas <bodhish@gmail.com>
2018-09-14 00:21:16 +0530

    Add custom implementation of splash Screen

--- App.js
+++ App.js
@@ -2,23 +2,37 @@ import React from "react";
 import { createStackNavigator } from "react-navigation";
 import Reminder from "./reminder";
 import AddReminder from "./addReminder";
+import Notify from "./notify";
 import PropTypes from "prop-types";
 
-const RootStack = createStackNavigator(
-  {
-    Home: Reminder,
-    AddReminder: AddReminder
-  },
-  {
-    initialRouteName: "Home"
-  }
-);
-
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialRouteName: "Home"
+    };
+  }
+
   render() {
+    const RootStack = createStackNavigator(
+      {
+        Home: Reminder,
+        AddReminder: AddReminder,
+        Notify: Notify
+      },
+      {
+        initialRouteName: "Home"
+      }
+    );
+    this.props.newID = this.props.alarmID;
     console.log("Alarm ID " + this.props.alarmID);
+    console.log(this.props.alarmID == null);
     console.log("Missed Alarm ID " + this.props.missedAlarms);
-    return <RootStack />;
+    return this.props.alarmID ? (
+      <Notify alarmID={this.props.alarmID} />
+    ) : (
+      <RootStack />
+    );
   }
 
   static propTypes = {
---
+++ notify.js
@@ -0,0 +1,53 @@
+import React from "react";
+import {
+  View,
+  TouchableOpacity,
+  AsyncStorage,
+  ToastAndroid,
+  Text,
+  StyleSheet
+} from "react-native";
+import PropTypes from "prop-types";
+import GestureRecognizer from "react-native-swipe-gestures";
+
+class Notify extends React.Component {
+  navigation() {
+    this.props.navigation.navigate("Home");
+  }
+  render() {
+    console.log(this.props.newId);
+
+    return (
+      <View>
+        <TouchableOpacity onPress={this.navigation.bind(this)}>
+          <View style={styles.app__cards}>
+            <Text>{this.props.alarmID}</Text>
+          </View>
+          <View>
+            <Text>This Is a Test Reminder</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+Notify.propTypes = {
+  alarmID: PropTypes.string
+};
+const styles = StyleSheet.create({
+  app__cards: {
+    flexDirection: "row",
+    height: 150,
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 10,
+    marginBottom: 10,
+    backgroundColor: "#16CB93",
+    maxHeight: 200,
+    borderColor: "#fff",
+    borderWidth: 0,
+    borderRadius: 20
+  }
+});
+
+module.exports = Notify;
