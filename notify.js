bb9622245ee5f07bae403f94db1310595507a4bb
Bodhish Thomas <bodhish@gmail.com>
2018-09-14 01:58:54 +0530

    Revamp the nofication view

--- notify.js
+++ notify.js
@@ -5,6 +5,7 @@ import {
   AsyncStorage,
   ToastAndroid,
   Text,
+  Image,
   BackHandler,
   StyleSheet
 } from "react-native";
@@ -26,21 +27,58 @@ class Notify extends React.Component {
     );
   }
   handleBackButtonClick() {
-    BackHandler.exitApp();
-    return true;
+    // BackHandler.exitApp();
+    // return true;
   }
 
   render() {
-    console.log(this.props.newId);
-
     return (
-      <View>
-        <TouchableOpacity onPress={this.handleBackButtonClick.bind(this)}>
-          <View style={styles.app__cards}>
-            <Text>{this.props.alarmID}</Text>
-          </View>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#16CB93"
+        }}
+      >
+        <View style={styles.app__notifyHeader}>
+          <Text
+            style={{
+              color: "#fff",
+              paddingLeft: 10,
+              fontWeight: "bold",
+              marginTop: 10,
+              marginBottom: 10,
+              fontSize: 28
+            }}
+          >
+            REMINDER
+          </Text>
+        </View>
+        <View style={styles.app__notify}>
           <View>
-            <Text>This Is a Test Reminder</Text>
+            <Image
+              source={require("./res/logo.png")}
+              style={{ width: 150, height: 150 }}
+            />
+            <Text>Reminder Id {this.props.alarmID}</Text>
+            <Text>Reminder Name {this.props.alarmID}</Text>
+          </View>
+        </View>
+        <TouchableOpacity
+          style={styles.app__notifyCloseButton}
+          onPress={this.handleBackButtonClick.bind(this)}
+        >
+          <View>
+            <Text
+              style={{
+                paddingLeft: 10,
+                fontWeight: "bold",
+                marginTop: 10,
+                marginBottom: 10,
+                fontSize: 18
+              }}
+            >
+              CLOSE REMINDER
+            </Text>
           </View>
         </TouchableOpacity>
       </View>
@@ -51,15 +89,37 @@ Notify.propTypes = {
   alarmID: PropTypes.string
 };
 const styles = StyleSheet.create({
-  app__cards: {
+  app__notifyHeader: {
+    flex: 1,
     flexDirection: "row",
-    height: 150,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#16CB93"
+  },
+  app__notify: {
+    flex: 5,
     marginLeft: 20,
+    alignItems: "center",
+    justifyContent: "center",
     marginRight: 20,
     marginTop: 10,
     marginBottom: 10,
-    backgroundColor: "#16CB93",
-    maxHeight: 200,
+    backgroundColor: "#fff",
+
+    borderColor: "#fff",
+    borderWidth: 0,
+    borderRadius: 20
+  },
+  app__notifyCloseButton: {
+    flex: 1,
+    marginLeft: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: 20,
+    marginTop: 40,
+    marginBottom: 20,
+    backgroundColor: "#fff",
+
     borderColor: "#fff",
     borderWidth: 0,
     borderRadius: 20
