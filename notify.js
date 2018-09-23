8264e83540f0455528fcdc08f2ce8909b4f94c05
Bodhish Thomas <bodhish@gmail.com>
2018-09-23 20:57:17 +0530

    Rewamp notification view to display reminders based on time

--- notify.js
+++ notify.js
@@ -5,17 +5,22 @@ import {
   AsyncStorage,
   Text,
   Image,
+  ScrollView,
   BackHandler,
-  StyleSheet
+  StyleSheet,
+  ToastAndroid
 } from "react-native";
 import PropTypes from "prop-types";
-import GestureRecognizer from "react-native-swipe-gestures";
+import NotifyCards from "./notifyCards";
+
 import {
   PlaySound,
   StopSound,
   PlaySoundRepeat,
   PlaySoundMusicVolume
 } from "react-native-play-sound";
+
+import AndroidAlarms from "react-native-android-alarms";
 
 class Notify extends React.Component {
   constructor(props) {
@@ -24,12 +29,17 @@ class Notify extends React.Component {
       name: "",
       morning: false,
       afternoon: false,
-      night: false
+      night: false,
+      reminders: [],
+      morning_time: "08:00",
+      afternoon_time: "12:00",
+      night_time: "20:00"
     };
   }
+
   componentDidMount = () => {
-    PlaySoundRepeat("bells");
-    // this.getData();
+    // PlaySoundRepeat("bells");
+    this.getData();
   };
 
   // TO DO: Setup Sound, Vibration< Display Alarm Details
@@ -51,21 +61,65 @@ class Notify extends React.Component {
     return true;
   }
 
+  handleSnooze() {
+    ToastAndroid.show("Alarm Snoozed for 30 mins", ToastAndroid.SHORT);
+    var today = new Date();
+    today.setDate(today.getMinutes() + 2);
+    AndroidAlarms.setAlarm(this.props.alarmID + "_s", today.valueOf(), false);
+    this.handleBackButtonClick();
+  }
+
   getData() {
-    AsyncStorage.getItem(this.props.alarmID.substr(0, 9), (err, result) => {
+    AsyncStorage.getItem("settings", (err, result) => {
       if (result) {
         var data = JSON.parse(result);
         this.setState({
-          name: data.name,
-          morning: data.morning,
-          afternoon: data.afternoon,
-          night: data.night
+          morning_time: data.morning_time,
+          afternoon_time: data.afternoon_time,
+          night_time: data.night_time
         });
       }
     });
+    AsyncStorage.getAllKeys((err, keys) => {
+      const newKeys = keys.filter(e => e !== "settings");
+      console.log(newKeys);
+      AsyncStorage.multiGet(newKeys).then(data => {
+        this.setState({ reminders: data });
+      });
+    });
+  }
+
+  checkTIme(key, value) {
+    var today = new Date();
+    const currentTIme = today.getHours();
+    if (currentTIme <= parseInt(this.state.morning_time.substr(0, 2))) {
+      return value.morning ? this.showCards(key, value) : null;
+    } else if (
+      currentTIme <= parseInt(this.state.afternoon_time.substr(0, 2))
+    ) {
+      return value.afternoon ? this.showCards(key, value) : null;
+    } else if (currentTIme <= parseInt(this.state.night_time.substr(0, 2))) {
+      return value.night ? this.showCards(key, value) : null;
+    } else {
+      return value.night ? this.showCards(key, value) : null;
+    }
+  }
+
+  showCards(key, value) {
+    return (
+      <NotifyCards
+        key={key}
+        id={key}
+        name={value.name}
+        morning={value.morning}
+        afternoon={value.afternoon}
+        night={value.night}
+      />
+    );
   }
 
   render() {
+    let reminderData = this.state.reminders;
     return (
       <View
         style={{
@@ -76,47 +130,73 @@ class Notify extends React.Component {
         <View style={styles.app__notifyHeader}>
           <Text
             style={{
-              color: "#fff",
-              paddingLeft: 10,
-              fontWeight: "bold",
-              marginTop: 10,
-              marginBottom: 10,
-              fontSize: 28
+              fontFamily: "FontAwesomeBold",
+              fontSize: 80,
+              color: "#fff"
             }}
           >
-            REMINDER
+            &#xf46b;
           </Text>
         </View>
         <View style={styles.app__notify}>
-          <View>
+          <View style={styles.app__CardHeader}>
             <Image
               source={require("./res/logo.png")}
-              style={{ width: 150, height: 150 }}
+              style={{ width: 50, height: 50 }}
             />
-            <Text>Reminder Name :{this.state.name}</Text>
-            <Text>Morning: {this.state.morning ? "True" : "False"}</Text>
-            <Text>Afternoon: {this.state.afternoon ? "True" : "False"}</Text>
-            <Text>Night: {this.state.night ? "True" : "False"}</Text>
-          </View>
-        </View>
-        <TouchableOpacity
-          style={styles.app__notifyCloseButton}
-          onPress={this.handleBackButtonClick.bind(this)}
-        >
-          <View>
             <Text
               style={{
-                paddingLeft: 10,
+                color: "#08cb88",
+                paddingLeft: 20,
                 fontWeight: "bold",
                 marginTop: 10,
                 marginBottom: 10,
-                fontSize: 18
+                fontSize: 28
               }}
             >
-              CLOSE REMINDER
+              YOUR REMINDERS
             </Text>
           </View>
-        </TouchableOpacity>
+          <ScrollView style={styles.app__notify}>
+            {reminderData.reverse().map((result, i, store) => {
+              key = store[i][0];
+              value = JSON.parse(store[i][1]);
+              return this.checkTIme(key, value);
+            })}
+          </ScrollView>
+        </View>
+        <View style={styles.app___notifyFooter}>
+          <TouchableOpacity
+            style={styles.app__notifyButton}
+            onPress={this.handleSnooze.bind(this)}
+          >
+            <View>
+              <Text
+                style={{
+                  fontFamily: "FontAwesome",
+                  fontSize: 40
+                }}
+              >
+                &#xf0f3;
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.app__notifyButton}
+            onPress={this.handleBackButtonClick.bind(this)}
+          >
+            <View>
+              <Text
+                style={{
+                  fontFamily: "FontAwesome",
+                  fontSize: 40
+                }}
+              >
+                &#xf00c;
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -126,39 +206,49 @@ Notify.propTypes = {
 };
 const styles = StyleSheet.create({
   app__notifyHeader: {
-    flex: 1,
+    flex: 2,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#16CB93"
   },
   app__notify: {
-    flex: 5,
-    marginLeft: 20,
-    alignItems: "center",
-    justifyContent: "center",
-    marginRight: 20,
-    marginTop: 10,
-    marginBottom: 10,
-    backgroundColor: "#fff",
-
-    borderColor: "#fff",
-    borderWidth: 0,
-    borderRadius: 20
+    flex: 6
   },
-  app__notifyCloseButton: {
-    flex: 1,
+  app__CardHeader: {
     marginLeft: 20,
+    marginRight: 20,
+    flexDirection: "row",
+    borderColor: "#fff",
+    backgroundColor: "#fff",
+    elevation: 6,
+    borderRadius: 20,
+    paddingLeft: 20,
+    marginBottom: 20,
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  app__notifyButton: {
+    marginLeft: 50,
     alignItems: "center",
     justifyContent: "center",
     marginRight: 20,
-    marginTop: 40,
     marginBottom: 20,
     backgroundColor: "#fff",
-
+    height: 100,
+    width: 100,
     borderColor: "#fff",
     borderWidth: 0,
-    borderRadius: 20
+    elevation: 6,
+    borderRadius: 25
+  },
+  app___notifyFooter: {
+    flex: 2,
+    marginTop: 30,
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 20,
+    flexDirection: "row"
   }
 });
 
